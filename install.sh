a8f28d9d4553928fe86a7a04f68977111583a028
Production <production@PRO-3545-Concert.local>
2023-01-11 15:27:36 +0100

    update installation script

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 set Lib =  "/Users/"`whoami`"/Library/Application Support/SuperCollider"
 
-open -a SuperCollider
+#open -a SuperCollider
 
 echo cp -r SuperCollider_add/TSupport/ "$Lib"
 cp -r SuperCollider_add/TSupport/ "$Lib"
 cp -r SuperCollider_add/Synthdefs/ "$Lib/Synthdefs"
 cp -r SuperCollider_add/Extensions/ "$Lib/Extensions"
-cp -r SuperCollider_add/Ambitools/ "$Lib/Extensions"
+cp -r SuperCollider_add/SC_Ambitools/ "$Lib/Extensions"
 mkdir "$Lib/sounds"
